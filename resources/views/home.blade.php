a2eb52f0c441233d2744d23386d2ccee36beb81e
Minh Thu <minhthu12234tgf@gmail.com>
2025-07-16 11:49:58 +0700

    Thêm .env và vendor vào repo

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Xin chào!</h3>
+        </div>
+        <div class="card-body">
+            Đây là giao diện trang chủ của bạn với AdminLTE.
+        </div>
+    </div>
+@endsection
