7657873f4525d8c7432b4839f5c749e6452e6a98
HV <conghoa247@gmail.com>
2025-07-18 10:18:47 +0700

    new-update

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,45 +13,53 @@ class DashboardController extends Controller
     // Lấy dữ liệu cho dashboard
     public function getDataForDashboard()
     {
-        $listNhanVien = NhanVien::select(
-            'id_nhanvien as id',
-            'ho_ten',
-            'trang_thai',
-            'vi_do',
-            'kinh_do',
-        )
-            ->where('trang_thai', '!=', 'Không trực tuyến')
-            ->whereNotNull('trang_thai')
-            ->get()
-            ->map(fn($nv) => [
-                'id' => $nv->id,
-                'ho_ten' => $nv->ho_ten,
-                'trang_thai' => $nv->trang_thai,
-                'vi_do' => $nv->vi_do,
-                'kinh_do' => $nv->kinh_do,
-                'anh_dai_dien' => './assets/img/vnpt.jpg',
-                'loai' => 'employee'
-            ]);
-
-        $listYeuCau = YeuCau::join('khach_hang', 'yeu_cau.id_khach_hang', '=', 'khach_hang.id_khachhang')
-            ->select(
-                'yeu_cau.id_yeu_cau as id',
-                'khach_hang.ho_ten as ho_ten',
-                'yeu_cau.trang_thai as trang_thai',
-                'yeu_cau.vi_do as vi_do',
-                'yeu_cau.kinh_do as kinh_do',
+        try {
+            $listNhanVien = NhanVien::select(
+                'id_nhanvien as id',
+                'ho_ten',
+                'trang_thai',
+                'vi_do',
+                'kinh_do',
             )
-            ->where('yeu_cau.trang_thai', 'Chờ xử lý')
-            ->get()
-            ->map(fn($yc) => [
-                'id' => $yc->id,
-                'ho_ten' => $yc->ho_ten,
-                'trang_thai' => $yc->trang_thai,
-                'vi_do' => $yc->vi_do,
-                'kinh_do' => $yc->kinh_do,
-                'anh_dai_dien' => './assets/img/user.png',
-                'loai' => 'customer'
-            ]);
+                ->where('trang_thai', '!=', 'Không trực tuyến')
+                ->whereNotNull('trang_thai')
+                ->get()
+                ->map(fn($nv) => [
+                    'id' => $nv->id,
+                    'ho_ten' => $nv->ho_ten,
+                    'trang_thai' => $nv->trang_thai,
+                    'vi_do' => $nv->vi_do,
+                    'kinh_do' => $nv->kinh_do,
+                    'anh_dai_dien' => './assets/img/vnpt.jpg',
+                    'loai' => 'employee'
+                ]);
+        } catch (\Exception $e) {
+            $listNhanVien = [];
+        }
+
+        try {
+            $listYeuCau = YeuCau::join('khach_hang', 'yeu_cau.id_khach_hang', '=', 'khach_hang.id_khachhang')
+                ->select(
+                    'yeu_cau.id_yeu_cau as id',
+                    'khach_hang.ho_ten as ho_ten',
+                    'yeu_cau.trang_thai as trang_thai',
+                    'yeu_cau.vi_do as vi_do',
+                    'yeu_cau.kinh_do as kinh_do',
+                )
+                ->where('yeu_cau.trang_thai', 'Chờ xử lý')
+                ->get()
+                ->map(fn($yc) => [
+                    'id' => $yc->id,
+                    'ho_ten' => $yc->ho_ten,
+                    'trang_thai' => $yc->trang_thai,
+                    'vi_do' => $yc->vi_do,
+                    'kinh_do' => $yc->kinh_do,
+                    'anh_dai_dien' => './assets/img/user.png',
+                    'loai' => 'customer'
+                ]);
+        } catch (\Exception $e) {
+            $listYeuCau = [];
+        }
 
         $allData = $listNhanVien->concat($listYeuCau); // concat nhanh hơn merge
 
